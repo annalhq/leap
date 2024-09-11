544161ccf5cd1c9ba5120cc47a40e0aa6de7d1a1
annalhq <annalhqshaikh@gmail.com>
2024-09-11 17:47:00 +0530

    added: rust

---
+++ rust/target/debug/deps/rust-673f2145d292e637.d
@@ -0,0 +1,5 @@
+/home/ann/programming/leap/rust/target/debug/deps/rust-673f2145d292e637: src/main.rs
+
+/home/ann/programming/leap/rust/target/debug/deps/rust-673f2145d292e637.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/ann/programming/leap/rust/target/debug/rust: /home/ann/programming/leap/rust/src/main.rs
