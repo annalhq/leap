fc8c8674b21e99e09443ed73bfcb005eadd99ede
annalhq <155549764+annalhq@users.noreply.github.com>
2024-09-16 00:11:55 +0530

    added: cpp

---
+++ cpp/cherno/basics.cpp
@@ -0,0 +1,9 @@
+// source file -> compiler -> executable
+// anything that begins with # is a preprocessor directive and is not compiled
+// main() is the entry point of the program
+
+#include <iostream>
+int main(){
+        // pushing hello world into the cout function
+        std::cout << "Hello World!" << std::endl;
+}
